6de3311cb6f052df0c3c90e2ee2fb412016b6f99
aliya.tang <aliya.w.tang@gmail.com>
2023-10-25 10:37:00 -0700

    try sms revert to old

--- app.js
+++ app.js
@@ -1,3 +1,29 @@
+function sendTextMsg() {
+    const accountSid = config.ACC_SID;
+    const authToken = config.AUTH_TOKEN;
+    //const accountSid = 'AC4b07fc38d18a961aab8bdf8379dd1607';
+    //const authToken = 'cb23c7fb2d619ac7d19fffa4ac96d608';
+    const msgBody = document.getElementById('announce').value;
+    const toNum =  document.getElementById('username').value;
+
+    const url = `https://api.twilio.com/2010-04-01/Accounts/${accountSid}/Messages.json`;
+
+    const formData = new FormData();
+    formData.append('Body', msgBody);
+    formData.append('To', toNum);
+    formData.append('From', '+18447565575');
+
+    fetch(url, {
+        method: 'POST',
+        headers: {
+            'Authorization': 'Basic ' + btoa(accountSid + ':' + authToken)
+        },
+        body: formData
+    })
+    .then(response => console.log(response))
+    .catch(error => console.error('Error:', error));
+}
+/*
 // define user data w/ usernames and numbers
 const userData = [
     { username: "aliya.tang", phoneNumber: "+13608238458" }
@@ -20,8 +46,8 @@ function sendTextMsg() {
         //const authToken = 'cb23c7fb2d619ac7d19fffa4ac96d608';
         const msgBody = document.getElementById('announce').value;
         const username = document.getElementById('username').value; // get entered username
-        const toNum =  findPhoneNumberByUsername(username); // retrieve user's phone number
-        //const toNum =  document.getElementById('number').value; 
+        //const toNum =  findPhoneNumberByUsername(username); // retrieve user's phone number
+        const toNum =  document.getElementById('username').value; 
         
         if (toNum) {
             const url = `https://api.twilio.com/2010-04-01/Accounts/${accountSid}/Messages.json`;
@@ -48,3 +74,4 @@ function sendTextMsg() {
 function displayMsg() {
 
 }
+*/
