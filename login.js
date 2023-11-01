3211ccd02132a30cc60d22032e9b9215d806e4b2
Raunak Mondal <spmondal08@gmail.com>
2023-10-31 18:44:50 -0700

    Update login.js

--- login.js
+++ login.js
@@ -2,34 +2,64 @@ const signupForm = document.getElementById("signup-form");
         const signinForm = document.getElementById("signin-form");
         const userList = document.getElementById("user-list");
         const userListContainer = document.getElementById("user-list-container");
-        // define user data w/ usernames and numbers
-        const userData = [
+        const searchInput = document.getElementById("search-user");
+        // Define user data with usernames and phone numbers
+        let userData = [
             { username: "user1", phoneNumber: "+1234567890" },
             { username: "user2", phoneNumber: "+9876543210" },
-            // add more user data...
+            // Add more user data...
         ];
-        function findPhoneNumberByUsername(username) {
-            // find user object with the matching username
-            const user = userData.find((user) => user.username === username);
-            if (user) {
-                return user.phoneNumber;
-            }
-            return null; // return null if username not found
-        }
         signupForm.addEventListener("submit", async function(event) {
             event.preventDefault();
             const username = document.getElementById("signup-username").value;
             const password = document.getElementById("signup-password").value;
             const phoneNumber = document.getElementById("signup-phone").value;
+            // Validate input and check if the username is already taken
+            if (!isUsernameAvailable(username)) {
+                alert("Username is already taken. Please choose another one.");
+                return;
+            }
             // Example: Add user data to the list
-            const userItem = document.createElement("li");
-            userItem.textContent = `Username: ${username}, Password: ${password}, Phone: ${phoneNumber}`;
-            userList.appendChild(userItem);
-
+            userData.push({ username, phoneNumber });
+            // Clear the form fields
+            document.getElementById("signup-username").value = "";
+            document.getElementById("signup-password").value = "";
+            document.getElementById("signup-phone").value = "";
+            updateUserDataList();
         });
         signinForm.addEventListener("submit", async function(event) {
             event.preventDefault();
             const username = document.getElementById("signin-username").value;
             const password = document.getElementById("signin-password").value;
-
+            // Implement user authentication logic here.
+            if (isUserAuthenticated(username, password)) {
+                alert("Sign-in successful!");
+            } else {
+                alert("Invalid username or password. Please try again.");
+            }
         });
+        // Function to check if a username is available
+        function isUsernameAvailable(username) {
+            return !userData.some(user => user.username === username);
+        }
+        // Function to update the user list
+        function updateUserDataList() {
+            userList.innerHTML = '';
+            userData.forEach(user => {
+                const userItem = document.createElement("li");
+                userItem.textContent = `Username: ${user.username}, Phone: ${user.phoneNumber}`;
+                userList.appendChild(userItem);
+            });
+        }
+        // Function to check if the user is authenticated (simplified)
+        function isUserAuthenticated(username, password) {
+            return userData.some(user => user.username === username);
+        }
+        // Function to search for users by username
+        function searchUsers() {
+            const searchQuery = searchInput.value.toLowerCase();
+            const filteredUsers = userData.filter(user => user.username.toLowerCase().includes(searchQuery));
+            updateUserDataList(filteredUsers);
+        }
+        // Initial user list display
+        updateUserDataList();
