eb0af7e5e1b49f1f697461dd7526053a6d9828fc
aliya.tang <aliya.w.tang@gmail.com>
2023-11-01 18:16:30 -0700

    app js

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
-import userData from './login.js';
+const userData = [
+    { username: "aliya.tang", phoneNumber: "+13608238458" },
+    { username: "user2", phoneNumber: "+9876543210" },
+    // add more user data...
+];
 
 function findPhoneNumberByUsername(username) {
     // find user object with the matching username
@@ -10,7 +14,9 @@ function findPhoneNumberByUsername(username) {
 }
 
 function sendTextMsg() {
-        const accountSid = config.ACC_SID;
+        //const accountSid = "AC4b07fc38d18a961aab8bdf8379dd1607";
+        const accountSid =  config.ACC_SID;
+        //const authToken = "4bb6a65eb8c05196244109f89043bffa"; 
         const authToken = config.AUTH_TOKEN;
         const msgBody = document.getElementById('announce').value;
         const username = document.getElementById('username').value; // get entered username
@@ -38,30 +44,3 @@ function sendTextMsg() {
             console.log("Username not found or phone number not available.");
         }
 }
-/*
-function sendTextMsg() {
-    const accountSid = config.ACC_SID;
-    const authToken = config.AUTH_TOKEN;
-    //const accountSid = 'AC4b07fc38d18a961aab8bdf8379dd1607';
-    //const authToken = 'cb23c7fb2d619ac7d19fffa4ac96d608';
-    const msgBody = document.getElementById('announce').value;
-    const toNum =  document.getElementById('username').value;
-
-    const url = `https://api.twilio.com/2010-04-01/Accounts/${accountSid}/Messages.json`;
-
-    const formData = new FormData();
-    formData.append('Body', msgBody);
-    formData.append('To', toNum);
-    formData.append('From', '+18447565575');
-
-    fetch(url, {
-        method: 'POST',
-        headers: {
-            'Authorization': 'Basic ' + btoa(accountSid + ':' + authToken)
-        },
-        body: formData
-    })
-    .then(response => console.log(response))
-    .catch(error => console.error('Error:', error));
-}
-*/
